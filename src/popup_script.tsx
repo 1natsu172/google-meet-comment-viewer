d8527bd612cbea9fb864e80a1d29a3e93167666b
1natsu172 <1natsu172@users.noreply.github.com>
2021-02-06 23:32:03 +0900

    feat: minimam popup app

---
+++ src/popup_script.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  return (
+    <div>
+      <h1>Options</h1>
+    </div>
+  )
+}
+
+const mountNode = document.getElementById('app')
+ReactDOM.render(<App />, mountNode)
